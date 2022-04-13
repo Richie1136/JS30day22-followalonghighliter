b12cb6907ccdbc28ca6a7abaa906b513520b87c6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-13 13:16:01 -0400

    Adding an event listener to all of the a tags

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const highlight = document.createElement('span')
 highlight.classList.add('highlight')
 
 document.body.append(highlight)
+
+const highlightLink = () => {
+  console.log('Highlight')
+}
+
+links.forEach((link) => {
+  link.addEventListener('mouseenter', highlightLink)
+})
